2d7fa1075b924524143dd61dbf83b07a41ae2e16
slavislavi <slavislavi1986@gmail.com>
2020-10-14 21:28:09 +0300

    feat: add extendedRepeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {  
+  
+ if (options.repeatTimes === undefined) {
+   return str + options.addition;
+ };
+ if (options.separator !== undefined) {
+    options.separator = String(options.separator);
+  } else {
+    options.separator = '+';
+  };
+ if (options.additionSeparator !== undefined) {
+   options.additionSeparator = String(options.additionSeparator);
+ } else {
+  options.additionSeparator = '|';
+ };
+
+  let result = '';
+  let addResult = '';
+
+ for (let i = 0; i < options.additionRepeatTimes - 1; i++) {
+  addResult += options.addition + options.additionSeparator;
+ };
+
+ if (options.additionRepeatTimes > 0) {
+   addResult += options.addition;
+ }
+
+ for (let i = 0; i < options.repeatTimes - 1; i++) {
+  result += str + addResult + options.separator;
+ };
+
+ if (options.repeatTimes > 0) {
+   result += str + addResult;
+ };
+ 
+ return result;
 };
   
--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 // require('./test/count-cats.test');
 // require('./test/carbon-dating.test');
 // require('./test/dream-team.test');
-// require('./test/extended-repeater.test');
-require('./test/what-season.test');
+require('./test/extended-repeater.test');
+// require('./test/what-season.test');
 // require('./test/vigenere-cipher.test');
 // require('./test/hanoi-tower.test');
 // require('./test/transform-array.test');
