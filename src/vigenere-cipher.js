d4dcdff97a560054aee5021575fede2d640a1c39
slavislavi <slavislavi1986@gmail.com>
2020-10-15 01:29:02 +0300

    feat: add vigenereCipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,49 @@
 const CustomError = require("../extensions/custom-error");
-
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
-}
+  constructor(direct = 1) {
+    this.direct = direct;
+  };
+  
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) throw new Error('ERROR!');
+    let result = [];
+    let iterator = -1;
+    for (let i = 0; i < message.length; i++) {
+      if (alphabet.indexOf(message[i].toUpperCase()) > -1) {
+        iterator++;
+        let index = (alphabet.indexOf(message[i].toUpperCase()) + alphabet.indexOf(key[iterator % key.length].toUpperCase())) % alphabet.length;
+        result.push(alphabet[index]);
+      } else {
+      result.push(message[i]);
+      };
+    };
+    if (this.direct) {
+      return result.join('');
+    } else {
+      return result.reverse().join('');
+    };
+  };
+
+  decrypt(message, key) {
+    if (message === undefined || key === undefined) throw new Error('ERROR!');
+    let result = [];
+    let iterator = -1;
+    for (let i = 0; i < message.length; i++) {
+      if (alphabet.indexOf(message[i].toUpperCase()) > -1) {
+        iterator++;
+        let index = (alphabet.indexOf(message[i].toUpperCase()) + alphabet.length - alphabet.indexOf(key[iterator % key.length].toUpperCase())) % alphabet.length;
+        result.push(alphabet[index]);
+      } else {
+      result.push(message[i]);
+      };
+    };
+    if (this.direct) {
+      return result.join('');
+    } else {
+      return result.reverse().join('');
+    };
+  };
+};
 
 module.exports = VigenereCipheringMachine;
